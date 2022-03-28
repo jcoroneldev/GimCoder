c2f9cec9b5823537f4ac10405e235bde7d1643ab
Javier Coronel <javiercoronel1@hotmail.com>
2022-03-28 12:30:53 -0300

    primer commit proyecto GimCoder

---
+++ proyecto_coder/GimCoder/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class profesor(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    turno = models.CharField(max_length=40)
+
+class cliente(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    peso = models.IntegerField()
+    altura = models.IntegerField()
+
+class rutina(models.Model):
+    repeticiones = models.IntegerField()
+    ejercicio1 = models.CharField(max_length=40)
+    ejercicio2 = models.CharField(max_length=40)
+    ejercicio3 = models.CharField(max_length=40)
+    ejercicio4 = models.CharField(max_length=40)
+    ejercicio5 = models.CharField(max_length=40)
+    ejercicio6 = models.CharField(max_length=40)
+
+class ejercicio(models.Model):
+    nombre = models.CharField(max_length=40)
+    detalle = models.CharField(max_length=80)
+    demostracion = models.CharField(max_length=40)
+    repeticiones = models.IntegerField()
+    duracion = models.TimeField()
+    
+
+
